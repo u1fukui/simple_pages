2d2386a085646595caefe3e9904a5f5a0e4f0c44
u1_fukui <u1.kobayashi@gmail.com>
2012-03-17 19:07:14 +0900

    GoogleMapAPIを使って、取得したツイートが本当に近くのものか確認できるようにした

--- nearby-tweet/twitter-search.js
+++ nearby-tweet/twitter-search.js
@@ -1,9 +1,29 @@
+var mapCanvas;
+
+function initialize() {
+    var latlng = new google.maps.LatLng(35.690921,139.700258);
+    var myOptions = {
+        zoom: 16,
+        center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    mapCanvas = new google.maps.Map(document.getElementById("map"), myOptions);
+
+}
+
+
 // formのボタン押されたら実行
 function search() {
+    var TWEET_COUNT = 100;
+    var CALLBACK_FUNCTION = "callbackFunc";
+
     // API URL生成
     var searchWord = document.form.text.value;
-    var apiUrl = "http://search.twitter.com/search.json?rpp=3&callback=callbackFunc"
-        + "&q=" + encodeURIComponent(searchWord);
+    var apiUrl = "http://search.twitter.com/search.json" +
+        "?rpp=" + TWEET_COUNT +
+        "&callback=" + CALLBACK_FUNCTION +
+        "&geocode=35.690921,139.700258,1km";
+    //"&q=" + encodeURIComponent(searchWord);
 
     callJSONP(apiUrl);
 }
@@ -18,23 +38,19 @@ function callJSONP(url) {
 
 // コールバック関数
 function callbackFunc(response) {
-    document.getElementById("response").innerHTML = objectToHtml(response);
-}
-
-// レスポンスを表示
-function objectToHtml(object) {
-    var html = "";
-    for (var prop in object) {
-        // プロパティ名
-        html += "<dt>" + prop + "</dt>";
-
-        // 値
-        if (typeof object[prop] == "object") {
-            var o = object[prop];
-            html += "<dd>" + objectToHtml(o) + "</dd>";
-        } else {
-            html += "<dd>" + object[prop] + "</dd>";
+    var results = response.results;
+    for (var i = 0; i < results.length; i++) {
+        var result = results[i];
+        if (result.geo != null) {
+            var pos = result.geo.coordinates;
+            var ll = new google.maps.LatLng(pos[0], pos[1]);
+            var marker = new google.maps.Marker({
+                position: ll,
+                map: mapCanvas,
+                draggable: false,
+                animation: google.maps.Animation.DROP,
+                icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=T|46b6d8|46b6d8'
+            });
         }
     }
-    return "<dl>" + html + "</dl>";
 }
