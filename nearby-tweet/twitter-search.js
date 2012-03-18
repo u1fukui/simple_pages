203e3fc0693e5860eb0fba0691f7c5d7a151a645
u1_fukui <u1.kobayashi@gmail.com>
2012-03-18 16:08:39 +0900

    GeolocationAPIを使用して現在地を取得する機能を追加

--- nearby-tweet/twitter-search.js
+++ nearby-tweet/twitter-search.js
@@ -1,29 +1,62 @@
+// GoogleMap
 var mapCanvas;
 
-function initialize() {
-    var latlng = new google.maps.LatLng(35.690921,139.700258);
+function main() {
+    // Geolocationに対応してるかチェック
+    if (navigator.geolocation == undefined) {
+        alert("位置情報の取得に未対応です");
+        return;
+    }
+
+    // 位置情報の取得
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+    // 成功したとき
+    function successCallback(pos) {
+        var lat = pos.coords.latitude;
+        var lon = pos.coords.longitude;
+
+        initializeMap(lat, lon);
+        search(lat, lon);
+    }
+
+    // 失敗したとき
+    function errorCallback(err) {
+        alert("失敗(" + err.code + ")" + err.message);
+
+        // 位置情報の初期値(新宿)
+        var DEFAULT_LATITUDE = 35.690921;
+        var DEFAULT_LONGITUDE = 139.700258;
+
+        initializeMap(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+        search(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+    }
+
+
+}
+
+// GoogleMap初期化
+function initializeMap(lat ,lon) {
+    // 地図生成
+    var latlng = new google.maps.LatLng(lat, lon);
     var myOptions = {
-        zoom: 16,
+        zoom: 15,
         center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     mapCanvas = new google.maps.Map(document.getElementById("map"), myOptions);
-
 }
 
-
 // formのボタン押されたら実行
-function search() {
+function search(lat, lon) {
     var TWEET_COUNT = 100;
     var CALLBACK_FUNCTION = "callbackFunc";
+    var RADIUS = "1km";
 
-    // API URL生成
-    var searchWord = document.form.text.value;
     var apiUrl = "http://search.twitter.com/search.json" +
         "?rpp=" + TWEET_COUNT +
         "&callback=" + CALLBACK_FUNCTION +
-        "&geocode=35.690921,139.700258,1km";
-    //"&q=" + encodeURIComponent(searchWord);
+        "&geocode=" + lat + "," + lon + "," + RADIUS;
 
     callJSONP(apiUrl);
 }
@@ -38,6 +71,9 @@ function callJSONP(url) {
 
 // コールバック関数
 function callbackFunc(response) {
+    var bounds = new google.maps.LatLngBounds();
+    var hitCount = 0; // geoの入っているツイート数
+
     var results = response.results;
     for (var i = 0; i < results.length; i++) {
         var result = results[i];
@@ -51,6 +87,13 @@ function callbackFunc(response) {
                 animation: google.maps.Animation.DROP,
                 icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=T|46b6d8|46b6d8'
             });
+            bounds.extend(ll);
+            hitCount++;
         }
+    }
+
+    // マーカーが全部入るように地図の拡大率を調整
+    if (hitCount > 0) {
+        mapCanvas.fitBounds(bounds);
     }
 }
